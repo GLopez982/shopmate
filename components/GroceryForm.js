637f9d48fada929dd52f0dae47510a806fd81cdc
Gerry <glopez92@mail.valenciacollege.edu>
2025-11-20 12:25:27 -0500

    uploading component folder and hook.js file structure

---
+++ components/GroceryForm.js
@@ -0,0 +1,31 @@
+import { useState, useRef } from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+
+
+export default function GroceryForm(){
+    const [inputValue, setInputValue] = useState("");
+    const input = useRef();
+
+    return(
+        <View>
+        
+        
+        
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 40, 
+        backgroundColor: 'white', 
+        marginRight: 10, 
+
+        // flex: 1, 
+        // display: 'flex',
+        // alignContent: 'center',
+        // alignItems: 'center', 
+
+    }
+})
